20a544c29daba2c0cf38ad27cdeaad2e96e4e89d
Phan Phat Tai <70010376+TaiTitans@users.noreply.github.com>
2025-04-05 10:54:05 +0700

    Update notification.go

--- internal/service/notification/notification.go
+++ internal/service/notification/notification.go
@@ -10,9 +10,9 @@ import (
 	"github.com/segmentio/kafka-go"
 	"go.uber.org/zap"
 
-	"github.com/augustus281/trackingcoin/global"
-	"github.com/augustus281/trackingcoin/internal/dto"
-	"github.com/augustus281/trackingcoin/internal/util"
+	"github.com/TaiTitans/TrackingCoin/global"
+	"github.com/TaiTitans/TrackingCoin/internal/dto"
+	"github.com/TaiTitans/TrackingCoin/internal/util"
 )
 
 const (
