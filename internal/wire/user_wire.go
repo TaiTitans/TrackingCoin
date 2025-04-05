7aaf168f1c983e0da88d555a1e50d3f8e62e8a4c
Phan Phat Tai <70010376+TaiTitans@users.noreply.github.com>
2025-04-05 10:57:00 +0700

    Update user_wire.go

--- internal/wire/user_wire.go
+++ internal/wire/user_wire.go
@@ -5,9 +5,9 @@ package wire
 import (
 	"github.com/google/wire"
 
-	handler "github.com/augustus281/trackingcoin/internal/handler"
-	repository "github.com/augustus281/trackingcoin/internal/repository/user"
-	service "github.com/augustus281/trackingcoin/internal/service/auth"
+	handler "github.com/TaiTitans/TrackingCoin/internal/handler"
+	repository "github.com/TaiTitans/TrackingCoin/internal/repository/user"
+	service "github.com/TaiTitans/TrackingCoin/internal/service/auth"
 )
 
 func InitAuthRouterHandler() (*handler.AuthHandler, error) {
